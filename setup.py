e5be1ea7603c86ae69b041908cd7eeceb7480810
Alberto Lezcano <alberto@ayax.estudio.net>
2020-03-09 21:46:20 +0100

    Fix release number

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
     name='pyaxis',
-    version='0.2.0',
+    version='0.3.0',
     author='Instituto Cántabro de Estadística',
     author_email='icane@cantabria.es',
     packages=['pyaxis', 'pyaxis.test'],
