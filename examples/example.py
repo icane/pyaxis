21e8a90d46004feddbdbd4a2f12c23a05661b8e0
Edwin Vehmaanperä <edwinvehmaanpera@gmail.com>
2021-03-28 12:41:34 +0300

    Return print for example...

--- examples/example.py
+++ examples/example.py
@@ -3,4 +3,5 @@ from pyaxis import pyaxis
 
 EXAMPLE_URL = 'http://www.ine.es/jaxiT3/files/t/es/px/2184.px'
 parsed_px = pyaxis.parse(EXAMPLE_URL, encoding='ISO-8859-2')
+print(parsed_px['DATA'])
 print(parsed_px['METADATA'])
