1f4cb92d46cb6427891fc57ade7c9e4182acb48c
Cecilio Alonso <alonso_cec@cantabria.es>
2021-04-08 14:25:57 +0200

    Release 0.3.4

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
     name='pyaxis',
-    version='0.3.3',
+    version='0.3.4',
     author='Instituto Cántabro de Estadística',
     author_email='icane@cantabria.es',
     packages=['pyaxis', 'pyaxis.test'],
