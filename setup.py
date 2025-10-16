fb054419b7c4f9d467a27b2a9ec4e8308fc7d5ac
lla11358 <lezcano_a@cantabria.es>
2025-10-16 09:20:13 +0200

    Fix issue #32

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
     name='pyaxis',
-    version='0.4.1',
+    version='0.4.2',
     author='Instituto Cántabro de Estadística & Swiss Data Science Center',
     author_email='icane@cantabria.es',
     packages=['pyaxis', 'pyaxis.test'],
